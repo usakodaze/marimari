6642b73b0188d953d29cd06148d2f48d547fd638
usakodaze <a15165383@gmail.com>
2024-11-28 14:23:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.addEventListener("load", function() {
+    // ページがロードされたら、ローダーを非表示にする
+    document.getElementById("loader").style.display = "none";  // ローダーを非表示にする
+    // クラス「loaded」を追加する処理（既存のコード）
     document.body.classList.add("loaded");
 });
